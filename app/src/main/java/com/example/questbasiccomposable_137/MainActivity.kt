cbc480b9a674c63009e6081ab7d05bbd2870c9c2
LingGrani <luthfiangger2510@gmail.com>
2024-10-18 08:30:21 +0700

    Adjust Layout to Center

--- app/src/main/java/com/example/questbasiccomposable_137/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_137/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_137.ui.theme.QuestBasicComposable_137Theme
@@ -33,7 +35,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicLayout(name: String, modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ){
         Text(
             text = "Hello $name!",
             modifier = modifier
